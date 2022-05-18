10dff18f5dbb665cb984f31c0901d7fd116d5369
Nisha Paudel Pandey <nshpoudel263@gmail.com>
2022-05-18 10:46:15 +1000

    react setup done

--- exercise-tracker-frontend/src/App.js
+++ exercise-tracker-frontend/src/App.js
@@ -1,7 +1,14 @@
+import { Button } from "react-bootstrap";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>helloe world </h1>
+      <i class="fa-solid fa-delete-right"></i>
+      <Button variant="primary"> delete me</Button>
+    </div>
+  );
 }
 
 export default App;
